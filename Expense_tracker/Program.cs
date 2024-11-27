20113b32fbc80c8086cf19a563378ac8ad9471b8
Otajonov Muhammadali <otajonovmuhammadali668@gmail.com>
2024-11-28 02:50:21 +0500

    Update Program.cs
    Program.cs Updating

--- Expense_tracker/Program.cs
+++ Expense_tracker/Program.cs
@@ -8,6 +8,27 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+      const string pathFile = @"D:\\HomeWork_PDP\\StringBulider\Order.txt";
+			OrderLibrary orderLibrary = new OrderLibrary();
+			while (true)
+			{
+				Console.WriteLine("Hit enter to add expense...");
+				string input = Console.ReadLine()?.ToLower();
+				if (input == "stop") break;
+
+				if (input == "enter")
+				{
+					orderLibrary.AddOrder();
+				}
+
+				orderLibrary.SaveToFileOrder();
+				Console.Clear();
+			}
+			using (StreamReader reader = new StreamReader(pathFile))
+			{
+				var linesOrder = reader.ReadToEnd();
+				Console.WriteLine(linesOrder);
+			}
+			Console.WriteLine("Thank you for your purchase :)");
     }
 }
